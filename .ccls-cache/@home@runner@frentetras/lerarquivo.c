518b01eb2bf9d12f3ff7e5292d2579b6d62efa80
Gabriel Godinho dos Santos <101431618+Gabriel-Godinho@users.noreply.github.com>
2022-12-09 19:59:28 +0000

    salvar aluno

--- .ccls-cache/@home@runner@frentetras/lerarquivo.c
+++ .ccls-cache/@home@runner@frentetras/lerarquivo.c
@@ -9,9 +9,14 @@ struct aluno {
 
 typedef struct aluno aln;
 
+void relatorio(){};
+
+void consulta(){};
+
 int main(int argc, char *argv[]) {
 
   FILE *arq;
+  aln listaAlunos[25];
   char Linha[100];
   char *result, *teste;
   int i;
@@ -26,13 +31,20 @@ int main(int argc, char *argv[]) {
   i = 1;
   while (!feof(arq))
   {
+
+    aln al;
+    
     result = fgets(Linha, 100, arq);
 
     teste = strtok(Linha, ";");
 	  printf("%s\n", teste);
+    al.id = (long int)teste;
 
     teste = strtok(NULL, ";");
     printf("%s\n", teste);
+    strcpy(al.nome, teste);
+
+    listaAlunos[i] = al;
     
     i++;
   }
